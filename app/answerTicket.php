7ab317158d27171fe51a1d6de15bf7c365eb9685
David Mignot <MisterGadou@Davids-MacBook-Pro.local>
2020-05-17 14:03:29 +1000

    added date updated in answer

--- app/answerTicket.php
+++ app/answerTicket.php
@@ -6,18 +6,21 @@ $ticket_id = $_REQUEST["i"];
 
 $answer = $_REQUEST["a"];
 
+$last_updated = date("Y/m/d");
+
  // Prepare an insert statement
- $sql = "UPDATE ticket SET message=?  WHERE ticket_id = (?)";
+ $sql = "UPDATE ticket SET message=?,last_updated=?  WHERE ticket_id = (?)";
  
  //echo "you got this ".$ticket_id;
 
   if($stmt = mysqli_prepare($link, $sql)){
       // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "si", $param_answer, $param_ticket_id  );
+      mysqli_stmt_bind_param($stmt, "ssi", $param_answer, $param_last_updated, $param_ticket_id  );
       
       // Set parameters
       $param_answer = $answer;
       $param_ticket_id = $ticket_id;
+      $param_last_updated = $last_updated;
   
       // Attempt to execute the prepared statement
       if(mysqli_stmt_execute($stmt)){
